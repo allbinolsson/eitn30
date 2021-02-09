72a89f2e3aed65c494f5a46bdb8396690a7368b1
Albin Olsson <al1100ol-s@student.lu.se>
2021-02-09 15:04:39 +0100

    Added threads to transmit and receive messages over udp

--- device/tun_base.py
+++ device/tun_base.py
@@ -1,22 +1,39 @@
 import socket
+import threading
 from tuntap import TunTap
 
 RCV_IP = "192.168.10.221"
 MY_IP = "192.168.10.206"
 UDP_PORT = 4000
 
+def transmit(socket, msg):
+    print("Transmitting!")
+    while True:
+        socket.sendto(msg.encode(), (RCV_IP, UDP_PORT))
+
+def receive(socket):
+    print("Receiving!")
+    while True:
+        rcvd, addr = socket.recvfrom(1024)
+        print("Received: {}\tFrom address: {}".format(rcvd, addr))
+
 if __name__ == "__main__":
     txSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     rxSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     rxSocket.bind((MY_IP, UDP_PORT))
-
+    
     tun = TunTap(nic_type="Tun", nic_name="tun0")
     tun.config(ip="192.168.2.1", mask="255.255.255.0")
 
-    while True:
-        # buf = tun.read()
-        # txSocket.sendto(buf, (RCV_IP, UDP_PORT))
-        rcvd, addr = rxSocket.recvfrom(1024)
-        print("Received: {}\tFrom address: {}".format(rcvd, addr))
-        tun.write(rcvd)
+    rxThread = threading.Thread(target=receive, args=(rxSocket,))
+    txThread = threading.Thread(target=transmit, args=(txSocket, 'Hello mobile',))
+    rxThread.start()
+    txThread.start()
+
+    # while True:
+    #     # buf = tun.read()
+    #     # txSocket.sendto(buf, (RCV_IP, UDP_PORT))
+    #     rcvd, addr = rxSocket.recvfrom(1024)
+    #     print("Received: {}\tFrom address: {}".format(rcvd, addr))
+    #     tun.write(rcvd)
     tun.close()
--- device/tun_mobile.py
+++ device/tun_mobile.py
@@ -1,9 +1,21 @@
 import socket
+import threading
 from tuntap import TunTap
 
 RCV_IP = "192.168.10.206"
 MY_IP = "192.168.10.221"
 UDP_PORT = 4000
+
+def transmit(socket, msg):
+    print("Transmitting!")
+    while True:
+        socket.sendto(msg.encode(), (RCV_IP, UDP_PORT))
+
+def receive(socket):
+    print("Receiving!")
+    while True:
+        rcvd, addr = socket.recvfrom(1024)
+        print("Received: {}\tFrom address: {}".format(rcvd, addr))
 
 if __name__ == "__main__":
     txSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -12,6 +24,11 @@ if __name__ == "__main__":
 
     tun = TunTap(nic_type="Tun", nic_name="tun0")
     tun.config(ip="192.168.2.2", mask="255.255.255.0", gateway="192.168.2.1")
+
+    rxThread = threading.Thread(target=receive, args=(rxSocket,))
+    txThread = threading.Thread(target=transmit, args=(txSocket, 'Hello base!',))
+    rxThread.start()
+    txThread.start()
 
     while True:
         buf = tun.read()
